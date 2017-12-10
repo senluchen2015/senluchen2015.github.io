b36731d2349203ccfd0ca782d256d5c41110524c
senluchen2015 <senluchen2015@u.northwestern.edu>
2017-12-09 16:07:37 -0800

    change bio

--- src/bio/bio.jsx
+++ src/bio/bio.jsx
@@ -8,7 +8,7 @@ const Bio = () => {
                 I am a front-end engineer in San Francisco, currently at <a href="https://sightmachine.com">Sight Machine</a>.
             </p>
             <p className="forio-links">
-                A sample of my professional work:
+                A sample of my past professional work:
                 <li><a href="http://forio.com/products/role-playing-simulation/" target="_blank">Role-Playing Simulation</a></li>
                 <li><a href="http://forio.com/work/hbp-tip-of-the-iceberg/" target="_blank">Harvard - Tip of the Iceberg</a></li>
                 <li><a href="http://forio.com/work/wharton-alternate-reality-teaching-looking-glass/" target="_blank">Wharton - Looking Glass</a></li>
