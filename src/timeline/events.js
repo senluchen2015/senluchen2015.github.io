40e94ded07b90ae48243ecc383558104ca35017b
senluchen2015 <senluchen2015@u.northwestern.edu>
2017-12-13 20:24:34 -0800

    add sighmachine

--- src/timeline/events.js
+++ src/timeline/events.js
@@ -1,34 +1,39 @@
 const events = [
 	{
 		imgLink: require('static/img/westtown-school.png'),
-		date: 'June 2011',
+		date: 'June/2011',
 		text: 'Graduated from the Westtown School',
 	},
 	{
 		imgLink: require('static/img/c++.png'),
-		date: 'August 2011',
+		date: 'August/2011',
 		text: 'Learned my first programming language C++',
 	},
 	{
 		imgLink: require('static/img/sap-logo.png'),
-		date: 'June 2013',
+		date: 'June/2013',
 		text: 'Interned at SAP',
 	},
 	{
 		imgLink: require('static/img/civis-analytics.png'),
-		date: 'June 2014',
+		date: 'June/2014',
 		text: 'Interned at Civis Analytics',
 	},
 	{
 		imgLink: require('static/img/northwestern-wildcats-logo.png'),
-		date: 'June 2015',
+		date: 'June/2015',
 		text: 'Graduated from Northwestern University with a degree in Computer Science',
 	},
 	{
 		imgLink: require('static/img/forio-logo.png'),
-		date: 'Current',
+		date: 'Aug/2014 - May/2017',
 		text: 'Frontend engineer at Forio',
-	}
+	},
+	{
+		imgLink: require('static/img/sightmachine.svg'),
+		date: 'June/2017 - Current',
+		text: 'Frontend engineer at Sight Machine',
+	},
 ];
 
 export default events;
